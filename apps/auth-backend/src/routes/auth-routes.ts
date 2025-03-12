75d5dd21a8ea6238aca83f43228a2325571ab54d
abdihashii <abdirahman.haji.13@gmail.com>
2025-03-11 21:07:53 -0500

    feat: implement user registration route with password hashing

--- apps/auth-backend/src/routes/auth-routes.ts
+++ apps/auth-backend/src/routes/auth-routes.ts
@@ -3,5 +3,37 @@ import { Hono } from "hono";
 
 // Local imports
 import type { CustomEnv } from "@/types";
+import { hashPassword } from "@/utils";
+import { usersTable } from "@/db/schema";
 
 export const authRoutes = new Hono<CustomEnv>();
+
+authRoutes.post("/register", async (c) => {
+  try {
+    // Get db connection
+    const db = c.get("db");
+
+    // Get body from request of type application/json
+    const body = await c.req.json();
+    const { email, password } = body;
+
+    // Hash user password
+    const hashedPassword = await hashPassword(password);
+
+    const newUser = {
+      email,
+      hashedPassword,
+    };
+
+    // Add user to users table
+    await db.insert(usersTable).values(newUser);
+
+    return c.json(
+      {
+        email,
+        message: `Successfully added ${email} to db!`,
+      },
+      200
+    );
+  } catch (err) {}
+});
