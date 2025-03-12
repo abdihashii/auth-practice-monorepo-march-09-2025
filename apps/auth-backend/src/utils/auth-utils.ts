26cf339c5145e319e0d7468e571cfac514c51d6e
abdihashii <abdirahman.haji.13@gmail.com>
2025-03-11 21:12:03 -0500

    refactor: improve password hashing with secure Argon2 configuration

--- apps/auth-backend/src/utils/auth-utils.ts
+++ apps/auth-backend/src/utils/auth-utils.ts
@@ -1,11 +1,15 @@
 // Third-party imports
 import argon2 from "argon2";
 
-export const hashPassword = async (password: string) => {
-  try {
-    const hash = await argon2.hash(password);
-    return hash;
-  } catch (err) {
-    throw Error("Unable to hash password");
-  }
-};
+/**
+ * Hash a password using Argon2id (recommended by OWASP)
+ * Using recommended settings from: https://cheatsheetseries.owasp.org/cheatsheets/Password_Storage_Cheat_Sheet.html
+ */
+export async function hashPassword(password: string): Promise<string> {
+  return argon2.hash(password, {
+    type: argon2.argon2id,
+    memoryCost: 65536, // 64MB
+    timeCost: 3, // Number of iterations
+    parallelism: 4,
+  });
+}
