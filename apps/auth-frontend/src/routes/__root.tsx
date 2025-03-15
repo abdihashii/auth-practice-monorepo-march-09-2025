1ea17e3f15f425c6c50e1c463fc421750bf978a8
abdihashii <abdirahman.haji.13@gmail.com>
2025-03-15 02:11:33 -0500

    feat: integrate React Query and its devtools into the root route for improved data fetching and debugging

--- apps/auth-frontend/src/routes/__root.tsx
+++ apps/auth-frontend/src/routes/__root.tsx
@@ -1,13 +1,20 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 import { ThemeProvider } from "@/providers/theme-provider";
 
+const queryClient = new QueryClient();
+
 export const Route = createRootRoute({
   component: () => (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <Outlet />
-      <TanStackRouterDevtools />
+      <QueryClientProvider client={queryClient}>
+        <Outlet />
+        <TanStackRouterDevtools />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </ThemeProvider>
   ),
 });
