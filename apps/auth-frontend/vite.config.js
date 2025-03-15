9271088343f6e8f1120f6d15c8bbebb98f2bea30
abdihashii <abdirahman.haji.13@gmail.com>
2025-03-15 01:20:11 -0500

    chore: reorganize vite.config.js for improved readability and add path alias for src directory

--- apps/auth-frontend/vite.config.js
+++ apps/auth-frontend/vite.config.js
@@ -1,15 +1,24 @@
-import { defineConfig } from "vite";
-import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import viteReact from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact(), tailwindcss()],
+  plugins: [
+    TanStackRouterVite({ autoCodeSplitting: true }),
+    viteReact(),
+    tailwindcss(),
+  ],
   test: {
     globals: true,
     environment: "jsdom",
   },
-
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
