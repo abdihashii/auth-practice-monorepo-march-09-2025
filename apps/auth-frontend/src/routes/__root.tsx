863ae40a1ce63ff26d33d736f50daba498842037
abdihashii <abdirahman.haji.13@gmail.com>
2025-03-15 02:31:44 -0500

    feat: implement AuthContextProvider to manage user authentication state and provide context to the application

--- apps/auth-frontend/src/routes/__root.tsx
+++ apps/auth-frontend/src/routes/__root.tsx
@@ -1,8 +1,11 @@
+// Third-party imports
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
+// Local imports
+import { AuthContextProvider } from "@/providers/auth-context-provider";
 import { ThemeProvider } from "@/providers/theme-provider";
 
 const queryClient = new QueryClient();
@@ -11,7 +14,9 @@ export const Route = createRootRoute({
   component: () => (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <QueryClientProvider client={queryClient}>
-        <Outlet />
+        <AuthContextProvider>
+          <Outlet />
+        </AuthContextProvider>
         <TanStackRouterDevtools />
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
