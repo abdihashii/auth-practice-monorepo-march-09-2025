e544752632090248e389d988da1b598d6ea38cb0
abdihashii <abdirahman.haji.13@gmail.com>
2025-03-15 02:09:34 -0500

    feat: implement login API function to handle user authentication requests

---
+++ apps/auth-frontend/src/api/auth-apis.ts
@@ -0,0 +1,20 @@
+import { BASE_API_URL } from "@/constants";
+
+export const login = async (email: string, password: string) => {
+  try {
+    const res = await fetch(`${BASE_API_URL}/api/v1/auth/login`, {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+    });
+
+    if (!res.ok) {
+      throw new Error("Failed to login");
+    }
+
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
