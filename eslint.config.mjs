5fe9983841b0f664c7853ad3b6cc08e2cb354018
abdihashii <abdirahman.haji.13@gmail.com>
2025-03-24 00:04:23 -0500

    chore: refactor ESLint configuration to use createConfig function for streamlined setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,3 @@
-import antfu from '@antfu/eslint-config';
+import createConfig from './packages/eslint-config/create-config.js';
 
-export default antfu({
-  type: 'app',
-  react: true,
-  typescript: true,
-  formatters: true,
-  stylistic: {
-    indent: 2,
-    semi: true,
-    quotes: 'single',
-  },
-  ignores: ['.pnpm-store/*', 'apps/auth-backend/drizzle/**', '**/*.md'],
-}, {
-  rules: {
-    'ts/no-redeclare': 'off',
-    'ts/consistent-type-definitions': ['error', 'interface'],
-    'no-console': ['warn'],
-    'antfu/no-top-level-await': ['off'],
-    'node/prefer-global/process': ['off'],
-    'node/no-process-env': ['error'],
-    'perfectionist/sort-imports': ['error', {
-      tsconfigRootDir: '.',
-    }],
-    'unicorn/filename-case': ['error', {
-      case: 'kebabCase',
-      ignore: ['README.md'],
-    }],
-    'style/brace-style': ['error', '1tbs', {
-      allowSingleLine: true,
-    }],
-    'style/arrow-parens': ['error', 'always'],
-    'antfu/if-newline': ['off'],
-  },
-});
+export default createConfig();
