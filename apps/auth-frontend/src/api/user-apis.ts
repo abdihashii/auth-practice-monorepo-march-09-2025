a6fa15ca2643be7a1076839d16ba914b68975116
abdihashii <abdirahman.haji.13@gmail.com>
2025-04-08 19:33:58 -0500

    feat: add updateUserPassword function for password management
    - Implemented a new function to update a user's password, requiring the user ID and an object containing the old and new passwords.
    - Enhanced API documentation to include details about the new password update functionality, improving clarity for future developers.

--- apps/auth-frontend/src/api/user-apis.ts
+++ apps/auth-frontend/src/api/user-apis.ts
@@ -1,9 +1,13 @@
-import type { ApiResponse, UpdateUserDto } from '@roll-your-own-auth/shared/types';
+import type { ApiResponse, UpdateUserDto, UpdateUserPasswordDto } from '@roll-your-own-auth/shared/types';
 
 import { BASE_API_URL } from '@/constants';
 
 /**
  * Update user's profile information
+ *
+ * @param {string} userId - The ID of the user to update
+ * @param {UpdateUserDto} user - The user data to update
+ * @returns {Promise<ApiResponse<{ message: string }>>} The response from the API
  */
 export async function updateUser(userId: string, user: UpdateUserDto) {
   // Validate the user data
@@ -22,6 +26,36 @@ export async function updateUser(userId: string, user: UpdateUserDto) {
 
   if (!res.ok) {
     const error = response.error || { message: 'Failed to update user' };
+    throw new Error(error.message);
+  }
+
+  const { data } = response as { data: ApiResponse<{ message: string }> };
+
+  return data;
+}
+
+/**
+ * Update user's password
+ *
+ * @param {string} userId - The ID of the user to update
+ * @param {UpdateUserPasswordDto} passwords - The passwords to update
+ * (old_password, new_password)
+ * @returns {Promise<ApiResponse<{ message: string }>>} The response from the API
+ */
+export async function updateUserPassword(userId: string, passwords: UpdateUserPasswordDto) {
+  // Update the user's password
+  const res = await fetch(`${BASE_API_URL}/api/v1/users/${userId}/password`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(passwords),
+  });
+
+  const response = await res.json();
+
+  if (!res.ok) {
+    const error = response.error || { message: 'Failed to update user password' };
     throw new Error(error.message);
   }
 
