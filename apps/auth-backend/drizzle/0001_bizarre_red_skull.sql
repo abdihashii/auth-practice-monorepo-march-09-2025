b63ffbece7e7cc2ae1e6f7a3a1b5fbdf5ddc5d1f
abdihashii <abdirahman.haji.13@gmail.com>
2025-04-19 16:06:37 -0500

    chore: make prettier

--- apps/auth-backend/drizzle/0001_bizarre_red_skull.sql
+++ apps/auth-backend/drizzle/0001_bizarre_red_skull.sql
@@ -1,3 +1,9 @@
 ALTER TABLE "auth"."users" ENABLE ROW LEVEL SECURITY;--> statement-breakpoint
 ALTER TABLE "profiles" ENABLE ROW LEVEL SECURITY;--> statement-breakpoint
-CREATE POLICY "user_can_select_self" ON "auth"."users" AS PERMISSIVE FOR SELECT TO "app_user" USING ((select auth.current_user_id()) = id);
+CREATE POLICY "user_can_select_self" 
+ON "auth"."users" 
+AS PERMISSIVE 
+FOR SELECT TO "app_user" 
+USING (
+  (select auth.current_user_id()) = id
+);
