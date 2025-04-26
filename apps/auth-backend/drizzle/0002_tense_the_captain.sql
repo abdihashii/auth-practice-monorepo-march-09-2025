e4d7385a576e1edecac0a579dbced49a0ac5d627
abdihashii <abdirahman.haji.13@gmail.com>
2025-04-26 18:52:01 -0500

    feat: add functions for current user identification and service request checks
    - Introduced `auth.get_current_user_id()` to retrieve the current user's UUID based on session settings.
    - Added `auth.is_service_request()` to determine if the request is made in a service context.
    - Updated row-level security policy for the `auth.users` table to utilize these new functions for enhanced access control.

--- apps/auth-backend/drizzle/0002_tense_the_captain.sql
+++ apps/auth-backend/drizzle/0002_tense_the_captain.sql
@@ -1,3 +1,29 @@
+-- Create a function that will help identify the current user
+CREATE OR REPLACE FUNCTION auth.get_current_user_id()
+RETURNS uuid
+LANGUAGE sql STABLE
+AS $$
+  SELECT 
+    CASE 
+      WHEN current_setting('app.current_user_id', TRUE) = '' THEN NULL
+      WHEN current_setting('app.current_user_id', TRUE) IS NULL THEN NULL
+      ELSE current_setting('app.current_user_id', TRUE)::uuid
+    END;
+$$;
+
+-- Create a Function to Check if in Service Context
+CREATE OR REPLACE FUNCTION auth.is_service_request()
+RETURNS boolean
+LANGUAGE sql STABLE
+AS $$
+  SELECT 
+    CASE 
+      WHEN current_setting('app.is_service_request', TRUE) = 'true' THEN TRUE
+      WHEN current_setting('app.is_service_request', TRUE) = 't' THEN TRUE
+      ELSE FALSE
+    END;
+$$;
+
 CREATE POLICY "users_policy" ON "auth"."users" AS PERMISSIVE FOR ALL TO public USING (
       (select auth.get_current_user_id()) = "auth"."users"."id"
       OR auth.is_service_request() = TRUE
